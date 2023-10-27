fdefbbf8f35b14cfc86c47e51cd89c7d3bc1c2f9
gplorovg <gplorovg@gmail.com>
2023-10-27 17:04:52 +0300

    add sandbox

---
+++ sandbox.py
@@ -0,0 +1,103 @@
+import subprocess
+import os
+import signal
+import time
+import paho.mqtt.client as mqtt
+from obswebsocket import obsws, requests
+import json
+
+# obs websocket connection
+obs_host = "localhost"
+obs_port = 4455
+obs_password = "TKoxvvk9TPgJNkt4"
+obs = obsws(obs_host, obs_port, obs_password)
+
+
+def ping_sources() -> dict:
+    resp = dict()
+    scenes = obs.call(requests.GetSceneList())
+    scenes_names = [scene["sceneName"] for scene in scenes.datain["scenes"]]
+    for scene_name in scenes_names:
+        sources = obs.call(requests.GetSceneItemList(sceneName=scene_name))
+        sources_names = [source["sourceName"] for source in sources.datain["sceneItems"]]
+        resp[scene_name] = dict()
+        for source_name in sources_names:
+            source_activity = obs.call(requests.GetSourceActive(sourceName=source_name))
+            resp[scene_name][source_name] = {"videoActive": source_activity.datain["videoActive"],
+                                             "videoShowing": source_activity.datain["videoShowing"]}
+    return resp
+
+
+# find path to obs64.exe in disk C:\
+def find():
+    for root, _, files in os.walk("C:\\"):
+        if "obs64.exe" in files:
+            return os.path.join(root)
+
+
+if not os.path.isfile("config.conf"):
+    OBS_PATH = find()
+    with open("config.conf", "w") as f:
+        f.write(OBS_PATH)
+else:
+    with open("config.conf") as f:
+        OBS_PATH = f.readline()
+
+
+# check if obs is running before program was started
+check_obs = subprocess.check_output('tasklist /fi "IMAGENAME eq obs64.exe" /fo "CSV"').decode(encoding="windows-1251")\
+    .replace('"', '').split(",")
+
+# !!! DANGER
+if len(check_obs) == 9:
+    print("obs PID: ", check_obs[5])
+    os.kill(int(check_obs[5]), signal.SIGTERM)
+else:
+    print("No obs detected!")
+
+# start obs64.exe
+# !!! obs_process.pid - pid of shell
+obs_process = subprocess.Popen(OBS_PATH + "\\" + "obs64.exe", cwd=OBS_PATH)
+
+
+# mqtt connection
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+    client.subscribe("/autostream")
+
+
+def publish(client, topic):
+    obs.connect()
+    msg = json.dumps(ping_sources())
+    obs.disconnect()
+    result = client.publish(topic, msg)
+    status = result[0]
+    if not status:
+        print(f"Send {msg} to {topic}")
+    else:
+        print(f"Failed to send message to topic {topic}")
+
+
+def on_message(client, userdata, msg):
+    if json.loads(msg.payload) == "PING_OBS":
+        publish(client, msg.topic)
+
+
+topic = "/autostream"
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+client.username_pw_set("recorder", "recorder2020")
+# connect_async to allow background processing
+client.connect_async("172.18.130.40", 1883, 60)
+
+client.loop_start()
+while True:
+    time.sleep(1)
+    poll = obs_process.poll()
+    if poll is None:
+        print("It's alive!")
+    else:
+        print("NOT alive!")
+        # start obs64.exe
+        obs_process = subprocess.Popen(OBS_PATH + "\\" + "obs64.exe", cwd=OBS_PATH)
