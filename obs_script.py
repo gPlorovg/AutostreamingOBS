f4d402045d08df2fd03a73f14013b09fbc2603e3
gplorovg <gplorovg@gmail.com>
2023-11-01 07:00:24 +0300

    change obs script to mqtt +8

--- obs_script.py
+++ obs_script.py
@@ -1,19 +1,11 @@
 import os
 import json
-import subprocess
+from dotenv import load_dotenv
+import paho.mqtt.client as mqtt
 import obspython as obs
 
 
-global OBS_PING_FILE
-
-
-def find():
-    for root, _, files in os.walk("C:\\"):
-        if "obs_ping_sources.json" in files:
-            return os.path.join(root, "obs_ping_sources.json")
-
-
-def ping_sources():
+def ping_sources() -> dict:
     resp = dict()
     for source in obs.obs_enum_sources():
         source_name = obs.obs_source_get_name(source)
@@ -21,27 +13,63 @@ def ping_sources():
         if input_address:
             # if "rtsp://" in input_address:
             cut_address = input_address.split("//")[1].split("/")[0]
-            is_online = subprocess.call("ping -n 1 " + cut_address, shell=True)
+            is_online = os.system("ping -n 1 " + cut_address) == 0
 
             resp[source_name] = {
                 "address": input_address,
                 "is_online": is_online
             }
-    resp_json = json.dumps(resp)
+    return resp
 
-    with open(OBS_PING_FILE, "w") as f:
-        f.write(resp_json)
+
+# find path to obs64.exe in disk C:\
+def find():
+    for root, _, files in os.walk("C:\\"):
+        if "autostreaming.env" in files:
+            return os.path.join(root + "\\autostreaming.env")
 
 
 def script_description():
     return "Ping all ip cameras"
 
 
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+    client.subscribe("autostream/ping_sources")
+
+
+def publish(client, topic):
+    msg = json.dumps(ping_sources())
+    result = client.publish(topic, msg)
+    # status = result[0]
+    # if not status:
+    #     print(f"Send {msg} to {topic}")
+    # else:
+    #     print(f"Failed to send message to topic {topic}")
+
+
+def on_message(client, userdata, msg):
+    if json.loads(msg.payload) == "PING_OBS":
+        publish(client, msg.topic)
+
+
 def script_load(settings):
-    global OBS_PING_FILE
-    OBS_PING_FILE = find()
-    obs.timer_add(ping_sources, 5000)
+    topic = "autostream/ping_sources"
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_message = on_message
+
+    # get local variables
+    env_path = find()
+    load_dotenv(env_path)
+    username = os.getenv("NAME")
+    password = os.getenv("PASSWORD")
+    client.username_pw_set(username, password)
+    # connect_async to allow background processing
+    client.connect_async("172.18.130.40", 1883, 60)
+    client.loop_start()
+    # obs.timer_add(ping_sources, 5000)
 
 
-def script_unload():
-    obs.timer_remove(ping_sources)
+# def script_unload():
+    # obs.timer_remove(ping_sources)
